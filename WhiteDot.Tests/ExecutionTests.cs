1437816719140e4da5974a4e891b8cc6cc8e051a
mario <marioskrlec222@proton.me>
2026-06-01 15:16:40 +0200

    Added first execution test

---
+++ WhiteDot.Tests/ExecutionTests.cs
@@ -0,0 +1,35 @@
+using System.Data.Common;
+using Npgsql;
+using WhiteDot.Tests.Namespace;
+
+namespace WhiteDot.Tests;
+
+public class ExecutionTests
+{
+    [Fact]
+    public async Task Should_Execute_Select_On_A_Given_Path()
+    {
+        string connectionString = "Host=localhost;Port=5432;Database=employees;Username=postgres;Password=password";
+
+        DbProviderFactory factory = NpgsqlFactory.Instance;
+        
+        var path = Path.Combine(
+            AppContext.BaseDirectory,
+            "testingYamls",
+            "execution.yaml");
+
+        var whiteDot = new WhiteDot(path, new Connection(connectionString, factory));
+        await whiteDot.ParseAsync();
+        var model = await whiteDot.ExecuteSingleAsync<EmployeeModel>("select.find_user", new Dictionary<string, object>()
+        {
+            {"id",  10001},
+        });
+
+        Assert.NotNull(model);
+        
+        Assert.NotEmpty(model.FirstName);
+        Assert.NotEmpty(model.LastName);
+        Assert.IsType<DateOnly>(model.BirthDate);
+        Assert.IsType<DateOnly>(model.HireDate);
+    }
+}
