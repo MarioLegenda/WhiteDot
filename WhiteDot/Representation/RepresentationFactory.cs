7e1ae1a33907f361c6e3412280da97cd57034009
mario <marioskrlec222@proton.me>
2026-06-12 11:37:07 +0200

    Refactored representation factory

--- WhiteDot/Representation/RepresentationFactory.cs
+++ WhiteDot/Representation/RepresentationFactory.cs
@@ -5,10 +5,9 @@ namespace WhiteDot.Representation;
 
 internal class RepresentationFactory
 {
-    private Root _representations;
+    private readonly Root _representations;
     
-    public RepresentationFactory(
-        Root representations)
+    public RepresentationFactory(Root representations)
     {
         this._representations = representations;
     }
@@ -19,37 +18,44 @@ internal class RepresentationFactory
 
         if (this._representations.Select != null)
         {
-            var select = this._representations.Select;
-            foreach (var (key, value) in select)
+            foreach (var (key, value) in this._representations.Select)
             {
-                var parameters = new Dictionary<string, List<string>>();
-                var sqlParameters = new List<string>();
-                foreach (Match match in Regex.Matches(value.Sql, @":\w+", RegexOptions.IgnoreCase))
-                {
-                    sqlParameters.Add(match.Value.TrimStart(':'));
-                }
-                parameters[key] = sqlParameters;
-                
-                var properties = new List<Property>();
-                foreach (var prop in value.Properties)
-                {
-                    var parts = prop.Split(" to ");
-
-                    var from = parts[0];
-                    var to = parts[1];
-                    var createdProperty = new Property(from, to);
-                    
-                    properties.Add(createdProperty);
-                }
-
                 representations[key] = new SelectRepresentation(
                     value.Sql,
-                    properties,
-                    parameters[key]
+                    this.createProperties(value.Properties),
+                    this.createParameters(value.Sql)
                 );
             }
         }
 
         return representations;
+    }
+
+    private List<string> createParameters(string sql)
+    {
+        var sqlParameters = new List<string>();
+        foreach (Match match in Regex.Matches(sql, @":\w+", RegexOptions.IgnoreCase))
+        {
+            sqlParameters.Add(match.Value.TrimStart(':'));
+        }
+
+        return sqlParameters;
+    }
+
+    private List<Property> createProperties(List<string> props)
+    {
+        var properties = new List<Property>();
+        foreach (var prop in props)
+        {
+            var parts = prop.Split(" to ");
+
+            var from = parts[0];
+            var to = parts[1];
+            var createdProperty = new Property(from, to);
+                    
+            properties.Add(createdProperty);
+        }
+
+        return properties;
     }
 }
